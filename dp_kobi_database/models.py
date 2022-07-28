6b294a8bc0b3ed56ca43d795cba831b5b311c3be
Kobi <harold.rockata@gmail.com>
2022-07-28 23:23:26 +0200

    Adding env and models setup for alembic, plus uri for BQ

--- dp_kobi_database/models.py
+++ dp_kobi_database/models.py
@@ -1,17 +1,23 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy import (
+    Boolean,
     Column,
     Date,
     Float,
     Integer,
+    MetaData,
     String,
+    DateTime
 )
 
-valorant_base = declarative_base()
+metadata = MetaData(schema="dp_kobi")
+valorant_base = declarative_base(metadata=metadata)
 
 
 class Maps(valorant_base):
+    __tablename__ = "maps"
+
     map_id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     location = Column(String, nullable=False)
@@ -20,6 +26,8 @@ class Maps(valorant_base):
 
 
 class Agents(valorant_base):
+    __tablename__ = "agents"
+
     agent_id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     origin = Column(String, nullable=False)
@@ -29,7 +37,23 @@ class Agents(valorant_base):
 
 
 class Patches(valorant_base):
+    __tablename__ = "patches"
+
     patch = Column(Integer, primary_key=True)
     release = Column(Date, nullable=False)
     size = Column(String, nullable=True)
     highlights = Column(String, nullable=True)
+
+
+class Matches(valorant_base):
+    __tablename__ = "valorant_matches"
+
+    match_id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime, nullable=False)
+    event = Column(String, nullable=True)
+    stakes = Column(String, nullable=True)
+    url = Column(String, nullable=True)
+    map_stats = Column(Boolean, nullable=False)
+    player_stats = Column(Boolean, nullable=False)
+
+
